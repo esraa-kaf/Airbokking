9a6d6f4a64b8cc0ff84a20a8aa5b7932cb9d89e3
esraa-kaf <esraakaf3@gmail.com>
2024-05-19 14:50:47 +0300

    change

--- app.js
+++ app.js
@@ -6,17 +6,17 @@ app.use(express.json({limit: '25mb'}));
 
 app.use(bodyParser.text({ limit: '5mb' })); // Parse text body with a limit of 5MB
 
-app.get("/",(req,res,next)=>{
-  return res.status(200).json({message:"welcome to my App airbooking"})
-})
+// app.get("/",(req,res,next)=>{
+//   return res.status(200).json({message:"welcome to my App airbooking"})
+// })
 const mongoose = require('mongoose')
 require('dotenv').config()
 const userRouter= require('./routes/userRouter') 
 const bookingRouter=require('./routes/bookingRouter')
 app.use([userRouter,bookingRouter])
-app.all('*',(req,res,next)=>{
-  res.status(404).send("in-valid routing")
-})
+// app.all('*',(req,res,next)=>{
+//   res.status(404).send("in-valid routing")
+// })
 const User=require("./models/userModel")
 const Booking =require("./models/bookingModel")
 const authMw  =require('./middleware/authMw')
